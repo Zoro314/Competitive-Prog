9596d9a2a68fea9a0ed07cab3e73a1b38b4e004a
Zoro314 <kiranchandra314@gmail.com>
2016-06-25 20:10:39 +0530

    Add files via upload

---
+++ Hackerearth/Prateek and his Friends.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m, n, amount ;
+	int invite = 0 ;
+
+	cin >> m ;
+	
+	while(m--)
+	{
+		cin >> n ;
+		cin >> amount ;
+		
+		
+	    int *arr1 = new int[n] ;        // create array dynamically
+		for(int i = 0; i < n ; i++ )
+		{
+			cin >> arr1[i] ;	
+		}
+		
+		for(int i = 0 ; i < n ; i++ )
+		{
+			int sum = 0 ;
+			invite = 0 ;
+
+		for( int j = i ; j < n ; j++)
+		{
+			sum += arr1[j] ;
+			if( sum > amount )	break ;
+			
+			if(sum == amount)
+			{
+				invite = 1 ;
+				break ;
+			}
+		 
+	//	 cout << sum  << " - " << amount << " " ;
+		}
+	//	cout << endl ;
+		
+			if( invite )
+			break ;
+		}
+		
+		if( invite )
+		{
+			cout << "YES" << endl ;
+		}
+		else
+		cout << "NO" << endl ;
+		
+		
+	    delete[] arr1;   // free the memory allocated
+
+	}
+
+
+
+    return 0;
+}
